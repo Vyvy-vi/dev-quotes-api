db5a40b5873e18bc5c27cb864827daa0a0cbbdc3
Vyvy-vi <vyom.j@protonmail.com>
2021-03-10 19:23:17 +0000

    fix: linted code

--- query.py
+++ query.py
@@ -11,30 +11,37 @@ AUTH = os.environ.get("HARPER_AUTH_TOKEN")
 url = "https://quote-api-vyvyvi.harperdbcloud.com"
 
 headers = {
-  'Content-Type': 'application/json',
-  'Authorization': f'Basic {AUTH}'
+    'Content-Type': 'application/json',
+    'Authorization': f'Basic {AUTH}'
 }
 
+
 def nosql_search(table, search_attr, search_val, get_attrs="*"):
-    payload = "{\n\t\"operation\":\"search_by_value\",\n\t\"schema\":\"dev\",\n\t\"table\":\"%s\",\n\t\"search_attribute\":\"%s\",\n\t\"search_value\":\"%s\",\n\t\"get_attributes\":[\"%s\"]\n}" % (table, search_attr, search_val, get_attrs)
+    payload = "{\n\t\"operation\":\"search_by_value\",\n\t\"schema\":\"dev\",\n\t\"table\":\"%s\",\n\t\"search_attribute\":\"%s\",\n\t\"search_value\":\"%s\",\n\t\"get_attributes\":[\"%s\"]\n}" % (
+        table, search_attr, search_val, get_attrs)
     res = requests.request("POST", url, headers=headers, data=payload)
     return json.loads(res.text.encode('utf8'))
 
 
 def sql_query(query):
     payload = "{\n\t\"operation\":\"sql\",\n\t\"sql\":\"%s\"\n}" % (query)
-    res = requests.request("POST", url, headers=headers, data = payload)
+    res = requests.request("POST", url, headers=headers, data=payload)
     return json.loads(res.text.encode('utf8'))
+
 
 def fetch_by_id(id):
     return sql_query(query=f"SELECT * FROM dev.quotes where id = {id}")
 
+
 def fetch_by_author(author):
-    return sql_query(query=f"SELECT * FROM dev.quotes where Author = '{author}'")
+    return sql_query(
+        query=f"SELECT * FROM dev.quotes where Author = '{author}'")
+
 
 def fetch_by_author_id(id):
     author = nosql_search('author', 'id', id, 'Author')[0]['Author']
     return fetch_by_author(author)
+
 
 def fetch_author(Author):
     res = nosql_search('author', 'Author', Author)
@@ -44,23 +51,25 @@ def fetch_author(Author):
         res = res[0]
         res['Author'] = res['Author'].title().replace('_', ' ')
         res = {
-                'name': res['Author'],
-                'author-id': res['id'],
-                'socials': {
-                    'Discord': res['Discord'],
-                    'Twitter': res['Twitter'],
-                    'Github': res['Github']
-                    }
-                }
+            'name': res['Author'],
+            'author-id': res['id'],
+            'socials': {
+                'Discord': res['Discord'],
+                'Twitter': res['Twitter'],
+                'Github': res['Github']
+            }
+        }
     return res
+
 
 def fetch_random_quote():
     return sql_query(query="SELECT * FROM dev.quotes WHERE id = %s" % (random.randint(0, quote_amount())))
 
+
 def fetch_many_random(num):
     QUOTES = []
     quotes_num = quote_amount()
-    if num < quotes_num:
+    if num > quotes_num:
         return sql_query(query="SELECT * FROM dev.quotes")
     else:
         nums = random.sample(range(0, quotes_num), num)
@@ -72,8 +81,8 @@ def fetch_many_random(num):
 def quote_amount():
     payload = "{\n\t\"operation\":\"describe_table\",\n\t\"table\":\"quotes\",\n\t\"schema\":\"dev\"\n}"
     res = requests.request("POST", url, headers=headers, data=payload)
-    return json.loads(res.text.encode('utf8'))['record_count'] - 1
+    return json.loads(res.text.encode('utf8'))['record_count']
 
-print(fetch_random_quote())
+# print(fetch_many_random(2))
 # print(fetch_by_author_id('1'))
 # print(fetch_by_author('eddie_jaoude'))
